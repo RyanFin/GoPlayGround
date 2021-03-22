d86827445ac408c3393e8a8094708e71aac237b4
RyanFin <ryanfinlayson125@hotmail.com>
2021-03-22 21:52:58 +0000

    Add heterogenous variadic func with interface{}

--- myvariadic_example.go
+++ myvariadic_example.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 // variadic functions allow the user to enter any amount of input params into a function
 // the amount can vary accordingly
@@ -9,6 +12,9 @@ func main() {
 	multipleNums(12, 3, 4, 5, 6, 7, 8, 9, 123, 12, 2, 3, 4)
 	multipleNums(0, 1, -2, -90)
 	multipleNums(1)
+
+	// heterogeneous -- interesting
+	heterogenFunction("hello", 'h', 1.0, 56, -86787)
 }
 
 func multipleNums(nums ...int) {
@@ -17,4 +23,12 @@ func multipleNums(nums ...int) {
 		fmt.Println("elem: ", i, " is ", e)
 	}
 
+}
+
+// heterogeneous variadic function using interface{} type
+func heterogenFunction(in ...interface{}) {
+	fmt.Println(in)
+	for _, e := range in {
+		fmt.Println("e: ", reflect.TypeOf(e))
+	}
 }
