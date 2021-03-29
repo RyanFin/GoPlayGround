536272a800ad79d3f48cd701ddba8e7832814590
RyanFin <ryanfinlayson125@hotmail.com>
2021-03-29 23:25:18 +0100

    Add linkedlist insertion code

--- goDataStructures/linkedlist/linkedList.go
+++ goDataStructures/linkedlist/linkedList.go
@@ -32,6 +32,29 @@ func (l LinkedList) printOutData() {
 	}
 }
 
+// insert data into LinkedList given a specified position in the list
+// The head node of the linkedlist marks the first parameter
+func (l *LinkedList) InsertNode(head *Node, data int, pos int) {
+	// generate a new node for insertion
+	newNode := &Node{data: data}
+
+	// set current Node to the head node n
+	currentNode := head
+
+	// create a new index
+	var index = 0
+
+	// while loop...
+	for index < pos-1 {
+		currentNode = currentNode.next
+		index++
+	}
+
+	newNode.next = currentNode.next
+	currentNode.next = newNode
+
+}
+
 func main() {
 	// create an empty linked list
 	myList := LinkedList{}
@@ -49,6 +72,7 @@ func main() {
 	myList.prepend(node4)
 	myList.prepend(node5)
 
+	myList.InsertNode(myList.head, 1997, 0)
 	myList.printOutData()
 
 }
