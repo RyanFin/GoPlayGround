cb597091eee78e371ce0441a2f2c7efd8504a31c
RyanFin <ryanfinlayson125@hotmail.com>
2021-04-14 20:03:04 +0100

    Add Doubly Linked List code

---
+++ goDataStructures/doublylinkedlist/doublylinkedlist.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type DoublyLinkedListNode struct {
+	data int32
+	next *DoublyLinkedListNode
+	prev *DoublyLinkedListNode
+}
+
+type DoublyLinkedList struct {
+	head *DoublyLinkedListNode
+	tail *DoublyLinkedListNode
+}
+
+func (doublyLinkedList *DoublyLinkedList) insertNodeIntoDoublyLinkedList(nodeData int32) {
+	node := &DoublyLinkedListNode{
+		next: nil,
+		prev: nil,
+		data: nodeData,
+	}
+
+	if doublyLinkedList.head == nil {
+		doublyLinkedList.head = node
+	} else {
+		doublyLinkedList.tail.next = node
+		node.prev = doublyLinkedList.tail
+	}
+
+	doublyLinkedList.tail = node
+}
+
+func printDoublyLinkedList(node *DoublyLinkedListNode, sep string, writer *bufio.Writer) {
+	for node != nil {
+		fmt.Fprintf(writer, "%d", node.data)
+
+		node = node.next
+
+		if node != nil {
+			fmt.Fprintf(writer, sep)
+		}
+	}
+}
+
+// Complete the sortedInsert function below.
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     data int32
+ *     next *DoublyLinkedListNode
+ *     prev *DoublyLinkedListNode
+ * }
+ *
+ */
+func sortedInsert(head *DoublyLinkedListNode, data int32) *DoublyLinkedListNode {
+
+	newNode := DoublyLinkedListNode{data: data}
+
+	// if list is empty
+	if head == nil {
+		head = &newNode
+	} else if head.data >= newNode.data {
+		// if the node is to be inserted at the beginning
+		// of the doubly linked list
+		newNode.next = head
+		newNode.next.prev = &newNode
+		head = &newNode
+	} else {
+		curr := head
+		// locate the node after which the new node
+		// is to be inserted
+		for curr.next != nil && curr.next.data < newNode.data {
+			curr = curr.next
+		}
+		/* Make the appropriate links */
+		newNode.next = curr.next
+
+		// if the new node is not inserted
+		// at the end of the list
+		if curr.next != nil {
+			newNode.next.prev = &newNode
+		}
+
+		curr.next = &newNode
+		newNode.prev = curr
+	}
+
+	return head
+
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	tTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	t := int32(tTemp)
+
+	for tItr := 0; tItr < int(t); tItr++ {
+		llistCount, err := strconv.ParseInt(readLine(reader), 10, 64)
+		checkError(err)
+
+		llist := DoublyLinkedList{}
+		for i := 0; i < int(llistCount); i++ {
+			llistItemTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+			checkError(err)
+			llistItem := int32(llistItemTemp)
+			llist.insertNodeIntoDoublyLinkedList(llistItem)
+		}
+
+		dataTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+		checkError(err)
+		data := int32(dataTemp)
+
+		llist1 := sortedInsert(llist.head, data)
+
+		printDoublyLinkedList(llist1, " ", writer)
+		fmt.Fprintf(writer, "\n")
+	}
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
