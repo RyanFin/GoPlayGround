b59d84ff88207dd6e341b6835e91ef0553484088
RyanFin <ryanfinlayson125@hotmail.com>
2021-06-29 01:53:47 +0100

    Only add one goroutine to wait group

--- waitgroupsv2.go
+++ waitgroupsv2.go
@@ -12,17 +12,19 @@ func main() {
 	// 5 go routines
 	sentences := []string{"hello world", "I am here", "To infinity and beyond", "well done", "Go Go Go"}
 
-	wg.Add(len(sentences))
-
 	for i := 0; i < len(sentences); i++ {
+		// add only a single go routine to the wait group at a time
+		wg.Add(1)
+		// run the function
 		go speak(i, &wg, sentences[i])
 	}
 
 	wg.Wait()
 }
 
-// Use waitgroup pointer
+// Use waitgroup pointer for go routine function
 func speak(id int, wg *sync.WaitGroup, s string) {
+	// state that the goroutine is finished
 	defer wg.Done()
 
 	fmt.Println("worker: ", id, " - ", s)
