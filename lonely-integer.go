b70beeee85676c2bce636bba1779bbcffe32e46f
RyanFin <ryanfinlayson125@hotmail.com>
2023-12-31 21:12:52 +0000

    add compare sort

---
+++ lonely-integer.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+func lonelyinteger(a []int32) int32 {
+	// Write your code here
+	m := make(map[int32]int32)
+
+	// add up the count for the amount of times a number appears in the array
+	for _, e := range a {
+		m[e] += 1
+	}
+
+	// print the map to check
+	fmt.Println(m)
+
+	// return the unique value (count == 1)
+	// return the key which contains a count of 1
+	res, _ := mapkey(m, 1)
+	fmt.Println(res)
+
+	return res
+}
+
+func mapkey(m map[int32]int32, value int32) (key int32, ok bool) {
+	for k, v := range m {
+		if v == value {
+			key = k
+			ok = true
+			return
+		}
+	}
+	return
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	aTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var a []int32
+
+	for i := 0; i < int(n); i++ {
+		aItemTemp, err := strconv.ParseInt(aTemp[i], 10, 64)
+		checkError(err)
+		aItem := int32(aItemTemp)
+		a = append(a, aItem)
+	}
+
+	result := lonelyinteger(a)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
